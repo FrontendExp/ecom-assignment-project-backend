bdd6d0c1ff990f47f4793cf2c66ec2612130e247
FrontendExp <avineshraghavendar@gmail.com>
2025-12-09 12:47:06 +0530

    backend change

--- config/db.js
+++ config/db.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/ecommerce", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    console.log("MongoDB Connected");
+    console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
-    console.error("MongoDB Connection Failed", error);
+    console.error("❌ MongoDB Connection Failed", error.message);
     process.exit(1);
   }
 };
