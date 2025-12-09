57af73fa606638c12fc1b3839e5c8658e8899eec
FrontendExp <avineshraghavendar@gmail.com>
2025-12-09 12:20:59 +0530

    correction

--- config/db.js
+++ config/db.js
@@ -15,21 +15,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect("mongodb://localhost:27017/ecommerce", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("MongoDB Connection Failed", error);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
--- models/cart.js
+++ models/cart.js
@@ -23,29 +23,3 @@ const cartSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("CartItem", cartSchema);
-
-const mongoose = require("mongoose");
-const cartSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: String,
-      required: true,
-    },
-    productName: {
-      type: String,
-      required: true,
-    },
-    productImage: String,
-    price: {
-      type: Number,
-      required: true,
-    },
-    quantity: {
-      type: Number,
-      default: 1,
-    },
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("CartItem", cartSchema);
